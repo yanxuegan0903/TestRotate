472a38396a37f8dd76ef71f5ef20359c2b817f44
[username] <yanxuegan0903@163.com>
2018-04-25 15:08:07 +0800

    Initial Commit

---
+++ TestRoTate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRoTate
+//
+//  Created by vsKing on 2018/4/25.
+//  Copyright © 2018年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
